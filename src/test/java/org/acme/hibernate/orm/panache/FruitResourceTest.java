3b98c2b166b18b35d11a89ce51471c762c9584fc
aequis <70807250+aequis@users.noreply.github.com>
2024-07-01 15:17:27 +0200

    Add more tests

--- src/test/java/org/acme/hibernate/orm/panache/FruitResourceTest.java
+++ src/test/java/org/acme/hibernate/orm/panache/FruitResourceTest.java
@@ -4,9 +4,11 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.preemptive;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.jboss.resteasy.reactive.RestResponse.StatusCode.NO_CONTENT;
+import static org.hamcrest.Matchers.emptyString;
+import static org.jboss.resteasy.reactive.RestResponse.StatusCode.*;
 
 class FruitResourceTest {
 
@@ -16,7 +18,7 @@ class FruitResourceTest {
                 .when()
                 .get("/fruits")
                 .then()
-                .statusCode(200)
+                .statusCode(OK)
                 .contentType("application/json")
                 .extract().response();
         assertThat(response.jsonPath().getList("name")).containsExactlyInAnyOrder("Cherry", "Apple", "Banana");
@@ -30,7 +32,7 @@ class FruitResourceTest {
                 .contentType("application/json")
                 .put("/fruits/1")
                 .then()
-                .statusCode(200)
+                .statusCode(OK)
                 .body(
                     containsString("\"id\":"),
                     containsString("\"name\":\"Pineapple\""));
@@ -43,5 +45,41 @@ class FruitResourceTest {
                 .delete("/fruits/1")
                 .then()
                 .statusCode(NO_CONTENT);
+    }
+
+    @Test
+    public void testCreatePear() {
+        given()
+                .when()
+                .body("{\"name\":\"Pear\"}")
+                .contentType("application/json")
+                .post("/fruits")
+                .then()
+                .statusCode(CREATED)
+                .body(
+                        containsString("\"id\":"),
+                        containsString("\"name\":\"Pear\"")
+                );
+    }
+
+    @Test
+    public void testEntityNotFoundForDelete() {
+        given()
+                .when()
+                .delete("/fruits/31535")
+                .then()
+                .statusCode(NOT_FOUND)
+                .body(emptyString());
+    }
+
+    @Test
+    public void testEntityNotFoundForUpdate() {
+        given()
+                .when()
+                .body("{\"name\": \"Pineapple\"}")
+                .put("/fruits/20359")
+                .then()
+                .statusCode(NOT_FOUND)
+                .body(emptyString());
     }
 }
