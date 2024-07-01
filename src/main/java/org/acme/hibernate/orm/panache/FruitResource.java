7bd54b464327cf0c5ba34a37ac63a517734b8aa4
aequis <70807250+aequis@users.noreply.github.com>
2024-07-01 15:05:04 +0200

    Add put endpoint

--- src/main/java/org/acme/hibernate/orm/panache/FruitResource.java
+++ src/main/java/org/acme/hibernate/orm/panache/FruitResource.java
@@ -4,9 +4,8 @@ import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.reactive.RestResponse;
 
 import java.util.List;
@@ -24,6 +23,20 @@ public class FruitResource {
     @GET
     public Uni<Fruit> getSingle(Long id) {
         return Fruit.findById(id);
+    }
+
+    @Path("/{id}")
+    @PUT
+    public Uni<Response> update(Fruit fruit, Long id) {
+        if (fruit == null || fruit.name == null) {
+            throw new WebApplicationException("Fruit name was not set on request", 422);
+        }
+        return Panache
+                .withTransaction(() -> Fruit.<Fruit> findById(id)
+                        .onItem().ifNotNull().invoke(entity -> entity.name = fruit.name)
+                )
+                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
+                .onItem().ifNull().continueWith(Response.ok().status(Response.Status.NOT_FOUND)::build);
     }
 
     @POST
