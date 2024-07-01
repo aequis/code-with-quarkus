460adcb38fe605b862c0e163b8ce3a81c85e65f4
aequis <70807250+aequis@users.noreply.github.com>
2024-07-01 15:05:13 +0200

    Add tests

---
+++ src/test/java/org/acme/hibernate/orm/panache/FruitResourceTest.java
@@ -0,0 +1,37 @@
+package org.acme.hibernate.orm.panache;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+
+class FruitResourceTest {
+
+    @Test
+    public void testListAllFruits() {
+        Response response = given()
+                .when()
+                .get("/fruits")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .extract().response();
+        assertThat(response.jsonPath().getList("name")).containsExactlyInAnyOrder("Cherry", "Apple", "Banana");
+    }
+
+    @Test
+    public void testPutFruit() {
+        given()
+                .when()
+                .body("{\"name\": \"Pineapple\"}")
+                .contentType("application/json")
+                .put("/fruits/1")
+                .then()
+                .statusCode(200)
+                .body(
+                    containsString("\"id\":"),
+                    containsString("\"name\":\"Pineapple\""));
+    }
+}
